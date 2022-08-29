5ea185b362c41d3f11794c82245bf3711f739e07
Tarik oz <a.tarik2005@gmail.com>
2022-08-29 14:23:50 +0300

    bittii

---
+++ app.js
@@ -0,0 +1,24 @@
+
+// function countBy(x, n) {
+//     let z = [];
+//   for (let i = 1; i <= n; i++) {
+//     a = x *i;
+
+//     z.push(a);
+//   }
+
+//   return z;
+// }
+
+// console.log(countBy(7, 4));
+
+// --------------------------
+// function between(a, b) {
+//     let x = [];
+//     for (let i = a; i <= b; i++) {
+//       x.push(i);
+//     }
+//     return x;
+   
+// }
+// console.log(between(-9,4))
